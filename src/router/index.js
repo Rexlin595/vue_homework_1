a755f0901cb88be4cfd43d75928d90c7b7febf27
Rexlin <karta2472826@gmail.com>
2025-09-06 18:02:23 +0800

    update homeworl_1

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import ProductListView from '../views/ProductList.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,7 +7,7 @@ const router = createRouter({
       // 2. 新增這個物件，將根路徑 '/' 指向您的商品列表元件
       path: '/',
       name: 'home', // 或 'products'
-      component: ProductListView,
+      component: () => import('../views/HomeView.vue'),
     },
     {
       path: '/about',
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// --- 這裡有變動 ---
+// 引入 Node.js 的 url 模組，用來處理檔案路徑
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      // --- 這裡有變動 ---
+      // 使用 import.meta.url 來取得目前檔案的路徑，並解析出 src 資料夾的絕對路徑
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
